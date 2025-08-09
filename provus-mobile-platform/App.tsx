f67b65564483c5efe80a113b8af074038127226e
YasmimZedelinski <yasmim.zedelinski@gmail.com>
2025-08-09 19:02:03 -0300

    feat: tela de login e configs iniciais

--- provus-mobile-platform/App.tsx
+++ provus-mobile-platform/App.tsx
@@ -1,28 +1,13 @@
 /**
  * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
+ * ...
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React from 'react';
+import LoginScreen from './src/screens/LoginScreen';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+function App(): React.JSX.Element {
+  return <LoginScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
